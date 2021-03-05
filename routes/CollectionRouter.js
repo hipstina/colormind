8a236d3ce970f1cd3640a29908ba36fd2fd27ed4
Christina <christinapadilla.cp@gmail.com>
2021-03-05 12:50:53 -0600

    fix spelling issue in routes

--- routes/CollectionRouter.js
+++ routes/CollectionRouter.js
@@ -5,14 +5,11 @@ Router.get('/get/collections', CollectionController.getCollections)
 Router.get('/view/collection/:id', CollectionController.getOneCollection)
 Router.post('/add/collection', CollectionController.createCollection)
 Router.delete('/delete/collection/:id', CollectionController.deleteCollection)
-Router.delete(
-  '/remove/collections/',
-  CollectionController.deleteCollectionsByName
-)
+// Router.delete(
+//   '/remove/collections/',
+//   CollectionController.deleteCollectionsByName
+// )
 Router.put('/edit/collection/:id', CollectionController.updateCollectionById)
-Router.post(
-  '/find/collection/:alias',
-  CollectionController.findCollectionByName
-)
+Router.get('/find/collection', CollectionController.findCollectionByName)
 
 module.exports = Router
